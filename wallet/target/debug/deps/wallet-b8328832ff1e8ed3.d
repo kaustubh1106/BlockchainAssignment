f136f59eff62772823302f2947bbe347849bcf98
Kaustubh <kaustubh@Kaustubhs-MacBook-Air.local>
2025-02-28 19:29:32 +0530

    rust wallet

---
+++ wallet/target/debug/deps/wallet-b8328832ff1e8ed3.d
@@ -0,0 +1,5 @@
+/Users/kaustubh/Desktop/assignment/wallet/target/debug/deps/libwallet-b8328832ff1e8ed3.rmeta: src/main.rs
+
+/Users/kaustubh/Desktop/assignment/wallet/target/debug/deps/wallet-b8328832ff1e8ed3.d: src/main.rs
+
+src/main.rs:
---
+++ wallet/target/debug/deps/wallet-ccae49fd686e380d.d
@@ -0,0 +1,5 @@
+/Users/kaustubh/Desktop/assignment/wallet/target/debug/deps/libwallet-ccae49fd686e380d.rmeta: src/main.rs
+
+/Users/kaustubh/Desktop/assignment/wallet/target/debug/deps/wallet-ccae49fd686e380d.d: src/main.rs
+
+src/main.rs:
